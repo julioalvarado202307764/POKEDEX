3bba292ae4be90af9c809b959de7c878bdaf7da0
Julio Eduardo Gustavo Alvarado Ramirez <128263541+julioalvarado202307764@users.noreply.github.com>
2023-05-11 08:51:20 -0600

    Login-Registrarse, teminado

---
+++ src/Principal/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Principal;
+
+/**
+ *
+ * @author Pistacho
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+        login.setLocationRelativeTo(null);
+        login.setResizable(false);
+        
+    }
+    
+}
